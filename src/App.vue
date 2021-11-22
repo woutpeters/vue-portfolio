38288810e944fa1cf1cb63929c42ce90c2db68f1
Wout Peters <wout.peters@concapps.nl>
2021-11-22 15:20:50 +0100

    add back-to-top button

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,13 @@
   <div :class="appTheme">
     <AppHeader />
     <router-view :theme="appTheme" />
+    <back-to-top visibleoffset="600" bottom="40px" right="30px" class="p-2 sm:p-4 bg-blue-500 hover:blue-blue-600 text-white rounded-lg shadow-md"><i data-feather="arrow-up"></i></back-to-top>
     <AppFooter />
   </div>
 </template>
 
 <script>
+import feather from 'feather-icons';
 import AppHeader from '@/components/AppHeader';
 import AppFooter from '@/components/AppFooter';
 
@@ -20,7 +22,13 @@ export default {
 		return {
 			appTheme: localStorage.getItem('theme'),
 		};
-	}
+	},
+  mounted() {
+    feather.replace();
+  },
+  updated() {
+    feather.replace();
+  }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -3,8 +3,9 @@ import App from './App.vue';
 import router from './router';
 import './assets/css/tailwind.css';
 import './assets/css/app.css';
+import BackToTop from 'vue-backtotop';
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(BackToTop).mount('#app')
 
 const appTheme = localStorage.getItem('theme');
 
