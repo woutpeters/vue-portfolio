9510ff21dabbc420ffeda782aceac9551a396c39
Wout Peters <wout.peters@concapps.nl>
2021-11-22 16:55:26 +0100

    responsive card changes

--- src/components/ProjectsGrid.vue
+++ src/components/ProjectsGrid.vue
@@ -4,15 +4,15 @@
       <p class="text-2xl sm:text-3xl font-semibold mb-3 text-ternary-dark dark:text-gray-50">Projects Portfolio</p>
       <p class="text-gray-500 dark:text-gray-400">Overzicht met de meest recente projecten</p>
     </div>
-    <div class="grid grid-cols-1 sm:grid-cols-4 mt-16 gap-4 xl:gap-8">
-      <router-link v-for="project in paginatedProjects" :key="project.id" :to="{name:'details',params:{slug:project.slug}}" class="rounded-lg shadow cursor-pointer mb-4 sm:mb-0 bg-gray-50 dark:bg-ternary-dark">
-      <div class="card-zoom">
-        <div class="card-zoom-image" v-if="project.image" :style="{'background-image': 'url(' + require(`@/assets/images/${project.image}`) + ')'}"></div>
-      </div>
-      <div class="flex flex-col items-center px-4 py-5">
-        <p class="text-xl text-ternary-dark dark:text-gray-50 font-semibold mb-2">{{ project.title }}</p>
-        <span class="text-sm text-gray-500 dark:text-gray-400">{{ project.category }}</span>
-      </div>
+    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 mt-16 gap-4 xl:gap-8">
+      <router-link v-for="project in paginatedProjects" :key="project.id" :to="{name:'details',params:{slug:project.slug}}" class="rounded-lg shadow cursor-pointer mb-4 sm:mb-0 bg-gray-50 dark:bg-ternary-dark flex flex-col">
+        <div class="card-zoom">
+          <div class="card-zoom-image" v-if="project.image" :style="{'background-image': 'url(' + require(`@/assets/images/${project.image}`) + ')'}"></div>
+        </div>
+        <div class="flex flex-col flex-1 items-center justify-center text-center px-4 py-5">
+          <p class="text-xl text-ternary-dark dark:text-gray-50 font-semibold mb-2">{{ project.title }}</p>
+          <span class="text-sm bg-gray-100 dark:bg-secondary-dark text-gray-400 rounded p-1 inline-flex items-center"><i data-feather="tag" class="w-4 h-4 mr-1"></i>{{ project.category }}</span>
+        </div>
       </router-link>
     </div>
     <div class="mt-10 sm:mt-20 flex justify-center">
@@ -22,6 +22,7 @@
 </template>
 
 <script>
+import feather from 'feather-icons';
 import projectList from "@/assets/data/list.json";
 
 export default {
@@ -44,6 +45,12 @@ export default {
       this.projectsToShow += this.projectsToAdd;
       this.showMore = this.projectsToShow < this.projects.length;
     }
+  },
+  mounted() {
+		feather.replace();
+	},
+  updated() {
+    feather.replace();
   }
 }
 </script>
