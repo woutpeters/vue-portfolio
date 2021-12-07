7502d33f91fb619829ca21a7723dcd59a9cba814
Wout Peters <wout.peters@concapps.nl>
2021-12-07 17:06:00 +0100

    added button class

--- src/components/AppBanner.vue
+++ src/components/AppBanner.vue
@@ -1,16 +1,18 @@
 <template>
   <section class="bg-cool-gray-50 dark:bg-blue-gray-800">
-    <div class="container flex flex-col sm:justify-between items-center sm:flex-row">
-      <div class="w-full sm:w-1/3 text-left">
-        <h1 class="text-3xl sm:text-4xl dark:text-gray-50 mb-3 text-center sm:text-left font-semibold">Hallo, ik ben Wout 👋</h1>
-        <p class="text-gray-500 dark:text-gray-400 mb-8 text-center sm:text-left leading-relaxed">Front-end Developer & Designer</p>
-        <div class="flex justify-center sm:justify-start mb-4 sm:mb-0">
-          <router-link class="inline-flex text-gray-700 bg-gray-100 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg transition-colors" :to="{name: 'contact'}">Contact</router-link>
-        </div>
-      </div>
-      <div class="lg:max-w-md lg:w-full md:w-1/2 w-5/6 sm:ml-3">
+    <div class="container flex flex-col sm:justify-between items-center py-32">
+      <div class="lg:max-w-md lg:w-full md:w-1/2 w-5/6">
         <img v-if="theme === 'light'" src="@/assets/images/designer.svg" class="object-cover object-center rounded" alt="hero">
         <img v-else src="@/assets/images/designer-dark.svg" class="object-cover object-center rounded" alt="hero">
+      </div>
+      <div class="bg-hero-home bg-cover w-full"></div>
+      <div class="w-full sm:w-1/3">
+        <h1 class="text-3xl sm:text-4xl dark:text-gray-50 mb-2 text-center font-semibold">Hallo, ik ben Wout 👋</h1>
+        <p class="sm:text-xl text-gray-500 dark:text-gray-400 mb-16 text-center leading-relaxed">Front-end Developer & Designer</p>
+        <div class="flex justify-center gap-4">
+          <router-link class="btn btn-primary text-lg" :to="{name: 'projects'}">Bekijk projecten</router-link>
+          <router-link class="btn btn-secondary text-lg" :to="{name: 'contact'}">Contact</router-link>
+        </div>
       </div>
     </div>
   </section>
